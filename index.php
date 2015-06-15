cd4e0a4f2e1ad97b31fba591233c3326eaad0c06
Raymon de Looff <raydelooff@gmail.com>
2015-06-15 15:00:31 +0200

    Added header and footer

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-	<title>Bootstrap 101 Template</title>
 
 	<!-- Bootstrap -->
 	<link href="css/screen.css" rel="stylesheet">
@@ -22,15 +21,34 @@
 	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
+
+    <title>PROFMOS</title>
 </head>
 
 <body>
 
-<h1>Hello, world!</h1>
+    <div class="jumbotron">
+        <div class="container">
+            <h1>PROFMOS</h1>
+        </div>
+    </div>
 
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js"></script>
+    <section id="content">
+        <div class="container">
+            <h1>Hello, world!</h1>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container">
+            <span>&copy; 2015 Delta Academy</span>
+        </div>
+    </footer>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js"></script>
+
 </body>
 </html>
