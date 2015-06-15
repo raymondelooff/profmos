70adbda3e2a7ffdb229f9a473a671a7f3005d5eb
Raymon de Looff <raydelooff@gmail.com>
2015-06-15 15:02:57 +0200

    Changed database host

--- includes/connectdb.php
+++ includes/connectdb.php
@@ -1,3 +1,3 @@
 <?php
 
-$database = new MysqliDb('delooff.synology.me', 'profmos', '6ReCXLg6B6hPwr2', 'profmos');
+$database = new MysqliDb('213.34.235.71', 'profmos', '6ReCXLg6B6hPwr2', 'profmos');
