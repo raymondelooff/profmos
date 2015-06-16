e17334e95ed121ff8ee107a56b787d4e4d0d8ed6
noor0035 <noor0035@hz.nl>
2015-06-16 13:36:06 +0200

    Formulieren aangepast

--- formulieren/monitoring.php
+++ formulieren/monitoring.php
@@ -8,9 +8,13 @@ require_once('../includes/connectdb.php');
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  	
+  	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+ 	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+ 	 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 
     <!-- Bootstrap -->
     <link href="../css/screen.css" rel="stylesheet">
