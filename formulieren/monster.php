4b87f5b9b1dc3f412f80c60308d83ac895327fd7
Lutgard <lutgardroelans@gmail.com>
2015-06-16 14:00:21 +0200

    Joost

--- formulieren/monster.php
+++ formulieren/monster.php
@@ -7,14 +7,22 @@ require_once('../includes/connectdb.php');
 <!DOCTYPE html>
 <html lang="nl">
     <head>
+<<<<<<< Updated upstream
      <meta charset="utf-8">
  	 <meta http-equiv="X-UA-Compatible" content="IE=edge">
  	 <meta name="viewport" content="width=device-width, initial-scale=1">
   	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-  
+
   	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+=======
+        <title>Monster Data</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+>>>>>>> Stashed changes
 
         <!-- Bootstrap -->
         <link href="../css/screen.css" rel="stylesheet">
@@ -39,24 +47,173 @@ require_once('../includes/connectdb.php');
             Datum: <div class="date">
                 <input type="text" class="form-control"><span class="input-group-addon"><i class="glyphicon glyphicon-th"></i></span>
             </div>
+            <label for="mosselgroep">Mosselgroep</label>
             <br>
-            Item: <input type="text">
+
+            <div class="form-group">
+            <input  class="form-control" type="text" id="mosselgroep" name="mosselgroep" value ="" maxlength="80" size="20">
+            </div>
             <br>
-            Item: <input type="text">
+
+            <div class="form-group">
+            <label for="bedrijf">Bedrijf</label>
             <br>
-            Item: <input type="text">
+            <input class="form-control" type="text" id="bedrijf" name="bedrijf" value ="" maxlength="80" size="20">
+            </div>
             <br>
-            Item: <input type="text">
+
+            <div class="form-group">
+            <label for="boot">Boot</label>
             <br>
-            Item: <input type="text">
+            <input  class="form-control" type="text" id="boot" name="boot" value ="" maxlength="80" size="20">
+            </div>
             <br>
-            Item: <input type="text">
+
+            <div class="form-group">
+            <label for="perceel">Perceel</label>
             <br>
-            Item: <input type="text">
+            <input  class="form-control" type="text" id="perceel" name="perceel" value ="" maxlength="80" size="20">
+            </div>
             <br>
-            Item: <input type="text">
+
+            <div class="form-group">
+            <label for="nummer">Nummer</label>
             <br>
-            Item: <input type="text">
+            <input class="form-control" type="text" id="nummer" name="nummer" value ="" maxlength="80" size="20">
+            </div>
+            <br>
+
+            <div class="form-group">
+            <label for="vak">Vak</label>
+            <br>
+            <input class="form-control" type="text" id="vak" name="vak" value ="" maxlength="80" size="20">
+            </div>
+            <br>
+
+            <div class="form-group">
+            <label for="bruto monster (g)">Bruto monster (g)</label>
+            <br>
+            <input class="form-control" type="text" id="bruto monster (g)" name="bruto monster (g)" value ="" maxlength="80" size="20">
+            </div>
+            <br>
+
+            <div class="form-group">
+            <label for="bustal">Bustal</label>
+            <br>
+            <input class="form-control" type="text" id="bustal" name="bustal" value ="" maxlength="80" size="20">
+            </div>
+            <br>
+
+            <div class="form-group">
+            <label for="slippers (g)">Slippers (g)</label>
+            <br>
+            <input class="form-control" type="text" id="slippers (g)" name="slippers (g)" value ="" maxlength="80" size="20">
+            </div>
+            <br>
+
+            <div class="form-group">
+            <label for="zeester (g)">Zeester (g)</label>
+            <br>
+            <input class="form-control" type="text" id="zeester (g)" name="zeester (g)" value ="" maxlength="80" size="20">
+            </div>
+            <br>
+
+            <div class="form-group">
+            <label for="pokken">Pokken</label>
+            <br>
+            <input class="form-control" type="text" id="pokken" name="pokken" value ="" maxlength="80" size="20">
+            </div>
+            <br>
+
+            <div class="form-group">
+            <label for="bus netto">Bus netto</label>
+            <br>
+            <input class="form-control" type="text" id="bus netto" name="bus netto" value ="" maxlength="80" size="20">
+            </div>
+            <br>
+
+            <div class="form-group">
+            <label for="kookmonster aantal">Kookmonster aantal</label>
+            <br>
+            <input class="form-control" type="text" id="kookmonster aantal" name="kookmonster aantal" value ="" maxlength="80" size="20">
+            </div>
+            <br>
+
+            <div class="form-group">
+            <label for="netto kookmonster (g)">Netto kookmonster (g)</label>
+            <br>
+            <input class="form-control" type="text" id="netto kookmonster (g)" name="netto kookmonster (g)" value ="" maxlength="80" size="20">
+            </div>
+            <br>
+
+            <div class="form-group">
+            <label for="vis totale monster">Vis totale monster</label>
+            <br>
+            <input class="form-control" type="text" id="vis totale monster" name="vis totale monster" value ="" maxlength="80" size="20">
+            </div>
+            <br>
+
+            <div class="form-group">
+            <label for="vis (%)">Vis (%)</label>
+            <br>
+            <input class="form-control" type="text" id="vis (%)" name="vis (%)" value ="" maxlength="80" size="20">
+            </div>
+            <br>
+
+            <div class="form-group">
+            <label for="opmerkingen">Opmerkingen</label>
+            <br>
+            <input class="form-control" type="text" id="opmerkingen" name="opmerkingen" value ="" maxlength="80" size="20">
+            </div>
+            <br>
+
+            <div class="form-group">
+            <label for="kroesnummer">Kroesnummer</label>
+            <br>
+            <input class="form-control" type="text" id="kroesnummer" name="kroesnummer" value ="" maxlength="80" size="20">
+            </div>
+            <br>
+
+            <div class="form-group">
+            <label for="kroes (g)">Kroes (g)</label>
+            <br>
+            <input class="form-control"  type="text" id="kroes (g)" name="kroes (g)" value ="" maxlength="80" size="20">
+            </div>
+            <br>
+
+            <div class="form-group">
+            <label for="kroes+vlees nat">Kroes+vlees nat</label>
+            <br>
+            <input class="form-control" type="text" id="kroes+vlees nat" name="kroes+vlees nat" value ="" maxlength="80" size="20">
+            </div>
+            <br>
+
+            <div class="form-group">
+            <label for="droog gewicht (g)">Droog gewicht (g)</label>
+            <br>
+            <input class="form-control" type="text" id="droog gewicht (g)" name="droog gewicht (g)" value ="" maxlength="80" size="20">
+            </div>
+            <br>
+
+            <div class="form-group">
+            <label for="asvrij droog gewicht (g)">Asvrij droog gewicht (g)</label>
+            <br>
+            <input class="form-control" type="text" id="asvrij droog gewicht (g)" name="asvrij droog gewicht (g)" value ="" maxlength="80" size="20">
+            </div>
+            <br>
+
+            <div class="form-group">
+            <label for="schelpen droog (g)">Schelpen droog (g)</label>
+            <br>
+            <input class="form-control" type="text" id="schelpen droog (g)" name="schelpen droog (g)" value ="" maxlength="80" size="20">
+            </div>
+            <br>
+
+            <div class="form-group">
+            <label for="gemiddelde lengte">Gemiddelde lengte</label>
+            <br>
+            <input class="form-control" type="text" id="gemiddelde lengte" name="gemiddelde lengte" value ="" maxlength="80" size="20">
+            </div>
             <br>
         </form>
 
--- formulieren/registratie_kweekactiviteiten.php
+++ formulieren/registratie_kweekactiviteiten.php
@@ -1,38 +1,13 @@
-<?php
-
-require_once('../includes/MysqliDb.php');
-require_once('../includes/connectdb.php');
-
-?>
 <!DOCTYPE html>
-<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-
-    <!-- CSS -->
-    <link href="http://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
-    <link href="css/screen.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-    <title>PROFMOS</title>
+  <title>Registratie kweerkactiviteiten</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 </head>
-
 <body>
-
-<?php
-
-include_once('../includes/header.php');
-
-?>
 
 <div class="container">
   <h2>Registratie kweekactiviteiten</h2>
@@ -65,7 +40,6 @@ include_once('../includes/header.php');
     <div class="form-group">
 	  <label for="gezaaid_als">Gezaaid als:</label>
 	  <select class="form-control" id="gezaaid_als">
-	    <option >MZI OS</option>
 		<option >MZI WAD</option>
 		<option >NJZAAD</option>
 		<option >VJZAAD</option>
