ad98ae1277717d5bf870062181946a3317b2d5ff
Raymon de Looff <raydelooff@gmail.com>
2015-06-18 18:40:35 +0200

    Added includes

--- selecteren-zaaiing.php
+++ selecteren-zaaiing.php
@@ -1,35 +1,26 @@
 <?php
-    //includes
-    require_once('../includes/MysqliDb.php');
-    require_once('../includes/connectdb.php');
+
+    // Includes
+    require_once('includes/MysqliDb.php');
+    require_once('includes/connectdb.php');
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <?php
 
-        <!-- CSS -->
-        <link href="http://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
-        <link href="css/screen.css" rel="stylesheet">
+            include_once('includes/head.php');
 
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
+        ?>
 
-        <title>PROFMOS</title>
+        <title>PROFMOS - Selecteren zaaiing</title>
     </head>
 
     <body>
         <?php
 
-            include_once('../includes/header.php');
+            include_once('includes/header.php');
 
         ?>
         <section id="content">
@@ -37,6 +28,7 @@
                 <div class="row">
                     <div class="col col-md-6">
                         <h2>Zaaiingen</h2>
+
                         <?php
                             $error_msg = "";
                             if (isset($_GET['zaaiingID'])) {
@@ -219,11 +211,15 @@
                 </div>
             </div>
         </section>
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <script src="/bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js"></script>
-        <script src="/js/bootstrap-datepicker.min.js"></script>
+
+        <?php
+
+            include_once('includes/footer.php');
+            include_once('includes/scripts.php');
+
+        ?>
+
+        <!-- Specific JS files here -->
         <script src="/js/invulformulieren.js"></script>
     </body>
 </html>
